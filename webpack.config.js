3875ab620ea148a52b9e1cb0b5fffa7660c13c68
Tabassum Khan <tabassumt1@gmail.com>
2026-06-17 09:29:28 +0200

    VA-1518 updates to latest packages

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
       {
         test: /\.js$/,
         include: [
-          path.resolve(__dirname, "scripts"),
-          path.resolve(__dirname, "entries")
+          path.resolve(__dirname, "src/scripts"),
+          path.resolve(__dirname, "src/entries")
         ],
         exclude: /node_modules/,
         loader: 'babel-loader'
@@ -35,7 +35,6 @@ module.exports = {
         { from: "../node_modules/mathjax/sre/", to: "sre" },
         { from: "../node_modules/@mathjax/mathjax-newcm-font/chtml/woff2/mjx-ncm-zero.woff2", to: "mathjax-newcm-font/chtml/woff2/" },
         { from: "../node_modules/@mathjax/mathjax-newcm-font/chtml/woff2/mjx-ncm-brk.woff2", to: "mathjax-newcm-font/chtml/woff2/" },
-        { from: "../node_modules/@mathjax/mathjax-newcm-font/chtml/woff2/mjx-ncm-n.woff2", to: "mathjax-newcm-font/chtml/woff2/" },
         { from: "../node_modules/@mathjax/mathjax-newcm-font/chtml/woff2/mjx-ncm-n.woff2", to: "mathjax-newcm-font/chtml/woff2/" },
         { from: "../node_modules/@mathjax/mathjax-newcm-font/chtml/woff2/mjx-ncm-lo.woff2", to: "mathjax-newcm-font/chtml/woff2/" }
       ],
