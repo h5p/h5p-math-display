2892b374dc590a54d85c268ecfe03592c499fd0f
Tabassum Khan <tabassumt1@gmail.com>
2026-06-18 11:20:30 +0200

    VA-1518 Convert MathDisplay to ES6 class

--- src/entries/dist.js
+++ src/entries/dist.js
@@ -1,4 +1,9 @@
-require('../scripts/config');
-// Load library
+import '../scripts/config.js';
+import MathDisplay from '../scripts/mathdisplay.js';
+
+// eslint-disable-next-line no-global-assign
 H5P = H5P || {};
-H5P.MathDisplay = require('../scripts/mathdisplay').default;
+H5P.MathDisplay = MathDisplay;
+
+// Fire up MathDisplay when the library is preloaded by H5P.
+new H5P.MathDisplay();
